c708b5d220d03b6d0a5baaaf8bc10e812bda0cb9
ben7147 <95097279+ben7147@users.noreply.github.com>
2024-04-30 18:02:35 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import anthropic
+
+client = anthropic.Anthropic(
+    # defaults to os.environ.get("ANTHROPIC_API_KEY")
+    api_key="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",
+)
+
+message = input("Kérdezz bármit! ~ ")
+
+message = client.messages.create(
+    model="claude-3-sonnet-20240229",
+    max_tokens=1000,
+    temperature=0,
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": message
+                }
+            ]
+        }
+    ]
+)
+print(message.content)
